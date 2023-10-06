a0e033a73280dbc47de79f7f0a5e36efe58e40a2
unknown <Akhilrampelli@gmail.com>
2023-10-06 10:48:59 +0530

    version4

--- DAY-8/COUNTING_SORT.py
+++ DAY-8/COUNTING_SORT.py
@@ -1,14 +1,12 @@
 l=list(map(int,input().split()))
 n=len(l)
-counter=[]
-for i in range(10):
-    counter.append(l.count(i))
-counter1=counter.copy()
+counter=[0 for i in range(10)]
+for i in range(n):
+    counter[l[i]]+=1
 for i in range(1,10):
     counter[i]+=counter[i-1]
 sortl=[0 for i in range(n)]
-for i in range(9,-1,-1):
-    for j in range(counter1[i]):
-        counter[i]-=1
-        l[counter[i]]=i
+for i in l[::-1]:
+    counter[i]-=1
+    l[counter[i]]=i
 print(l)
